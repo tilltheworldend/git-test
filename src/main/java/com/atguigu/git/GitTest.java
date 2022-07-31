e19e9b487b7cabf154072c27a0f6586dfc39720f
zhoulin <1026507536@qq.com>
2022-07-31 16:33:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhoulin
+ * @date 2022-07-31-16:24
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
